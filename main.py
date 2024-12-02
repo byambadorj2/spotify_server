2dc002b341230bb23cea576309657c73c3e548c1
byambadorj2 <byambadorj993@gmail.com>
2024-12-02 18:38:58 +0800

    fav, list fav, fav table

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from sys import prefix
 from fastapi import FastAPI
-from routes import auth
+from routes import auth,song
 from models.base import Base
 from database import engine
  
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth")
+app.include_router(song.router, prefix="/song")
   
 Base.metadata.create_all(engine)
